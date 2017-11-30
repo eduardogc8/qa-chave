8b7234105cdc21ced178dc521a19e842fa17ab1f
Eduardo <eduardogcortes8@gmail.com>
2017-11-30 12:45:29 -0200

    Adjustes

--- manager_dataset.py
+++ manager_dataset.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import xml.etree.ElementTree as et
-import qa_system
 import re
 
 path_questions = "data/questions.xml"
@@ -13,67 +12,51 @@ paths_documents = ['data/documents/folha94/', 'data/documents/folha95/',
 # paths_documents = ['data/documents/test/']
 
 
-# Verifica se os dados já estão indexados, se não estiverem é feito a indexação
-def run_mananger(): pass
-
-
-# Abre o arquivo dataset e retorna uma lista de perguntas em formato tree
-def dataset():
-    file = open(path_questions, 'r')
-    tree = et.parse(file)
+# Open questions file and return it tree
+def questions_tree():
+    tree = et.parse(path_questions)
     return tree.getroot()
 
 
-# Retorna todos os pares em um lista
-def pairs():
-    _, _, ret = valid_invalid_pairs()
+# Return a list with all questions in dataset
+def questions():
+    ret = []
+    count = 0
+    for question in questions_tree():
+        q = {}
+        q['id'] = count
+        q['id_org'] = question.attrib['id_org']
+        q['year'] = question.attrib['ano']
+        q['category'] = question.attrib['categoria']
+        q['type'] = question.attrib['tipo']
+        q['ling'] = question.attrib['ling_orig']
+        if u'restrição' in question.attrib:
+            q['restriction'] = question.attrib[u'restrição']
+        if 'restricao' in question.attrib:
+            q['restriction'] = question.attrib['restricao']
+        if q['restriction'] == 'NO': q['restriction'] = 'NONE'
+        if q['restriction'] == 'X': q['restriction'] = ''
+        
+        q['answers'] = []
+        q['extracts'] = []
+
+        for e in question:
+            if e.tag == 'texto':
+                q['question'] = e.text
+            if e.tag == 'resposta':
+                ans = {'answer':e.text, 'n':e.attrib['n'], 'doc':e.attrib['docid']}
+                ans['doc'] = validate_docid(ans['doc'])
+                q['answers'].append(ans)
+            if e.tag == 'extracto':
+                q['extracts'].append({'extract':e.text, 'n':e.attrib['n'], 'answer_n':e.attrib['resposta_n']})
+        count += 1
+        ret.append(q)
     return ret
 
 
-# Retorna todas os pares do dataset que tem ao menos uma resposta válida,
-# também, é retornado os pares inválidos
-# Válido significa: o par contém resposta e a resposta tem um docid de origem válida (FolhaSP ou Público)
-def valid_invalid_pairs():
-    ret = []  # Valid
-    ret2 = []  # Invalid
-    ret3 = []  # All
-    pairs = dataset()
-
-    tot = 0
-    for pair in pairs:
-        tot += 1
-        pair = tree_to_pair(pair)
-
-        if pair.question is None or pair.question.strip() == '':
-            continue
-        else:
-            valid_answers = []
-            valid_docid = []
-            for i in range(len(pair.correct_answers)):
-                answer = pair.correct_answers[i]
-                docid = pair.correct_docs_id[i]
-                new_docid = validate_docid(docid)
-                if new_docid is not None:
-                    valid_docid.append(new_docid)
-                    valid_answers.append(answer)
-            pair.correct_answers = valid_answers
-            pair.correct_docs_id = valid_docid
-            if len(pair.correct_answers) > 0:
-                ret.append(pair)
-            else:
-                ret2.append(pair)
-        ret3.append(pair)
-    # print 'Total Questions:\t'+str(len(pairs))
-    # print 'Valid Questions:\t'+str(len(ret))
-    print 'Total:',tot
-    print 'valid:',len(ret),'invalid:',len(ret2)
-    return ret, ret2, ret3
-
-
-# Se é um docid válido(FolhaSP ou Público) então retorna um docid no
-# formato do docid dos documentos se não retorna None
+# Check if a docid is valid (FolhaSP or Público) so return a docid in the
+# documents docid else return None
 def validate_docid(docid):
-    global aux
     if docid is None or len(docid.strip()) < 10:
         return None
     else:
@@ -88,40 +71,7 @@ def validate_docid(docid):
             return None
 
 
-# Passa a pergunta em formato tree para um objeto do tipo Question
-def tree_to_pair(tree_question):
-    p = qa_system.Pair()
-
-    if 'categoria' in tree_question.attrib:
-        p.question_category = tree_question.attrib['categoria']
-    if 'tipo' in tree_question.attrib:
-        p.question_type = tree_question.attrib['tipo']
-    if u'restrição' in tree_question.attrib:
-        p.question_restriction = tree_question.attrib[u'restrição']
-
-    p.correct_classification = pair_classification(p)
-
-    for t in tree_question:
-        if t.tag == 'texto':
-            if t.text is not None:
-                if type(t.text) == type(''): 
-                    p.question = t.text.decode('utf-8')
-                else:
-                    p.question = t.text
-        elif t.tag == 'resposta':
-            docid = None
-            if 'docid' in t.attrib:
-                docid = t.attrib['docid']
-            if t.text is not None and not t.text.strip() == '':
-                p.correct_answers.append(t.text.encode('utf-8'))
-                p.correct_docs_id.append(docid)
-        elif t.tag == 'extracto':
-            if t.text is not None:
-                p.extractos.append(t.text)
-    return p
-
-
-# Retorna a classificação da questão com base em sua categoria e tipo
+# Return the right question class based in category and type attributes
 def pair_classification(pair):
     t = pair.question_type
     c = pair.question_category
@@ -158,10 +108,3 @@ def pair_classification(pair):
     if c == 'TIME':
         return 'TIME'
     return c
-
-
-def treat(text):
-    if text is None:
-        return None
-    ret = text.replace('?', '').replace(u'«', '\"').replace(u'»', '\"')
-    return ret
